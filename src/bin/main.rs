f854512dd9af7d80e90c901e598672cdbcd7c228
s4h <a@b.c>
2023-09-13 12:45:24 +0100

    Add initial code

---
+++ src/bin/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+use rs_template::init_telemetry;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct App {
+
+}
+
+#[tokio::main]
+async fn main() {
+    init_telemetry();
+
+    let app = App::parse();
+
+    tracing::info!("hello world");
+    tracing::warn!("OH NO");
+    tracing::debug!("{:?}", app);
+    tracing::trace!("{:#?}", app);
+}
